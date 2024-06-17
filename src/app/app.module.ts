75c240f0a0ac3aab2799b0a52618b6a8889dcec3
Ganesh0333 <kottaliganesh1994@gmail.com>
2024-06-17 19:25:11 +0530

    initial setup completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreateComponent } from './components/create/create.component';
+import { EditComponent } from './components/edit/edit.component';
+import { ViewComponent } from './components/view/view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateComponent,
+    EditComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule,
