82569c7e93d0047990050cedeae87cf535ff52c6
Ganesh0333 <kottaliganesh1994@gmail.com>
2024-06-17 20:15:32 +0530

    create-communication-component-implemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EditCommunicationComponent } from './components/edit-communication/edit-communication.component';
+import { CreateCommunicationComponent } from './components/create-communication/create-communication.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EditCommunicationComponent,
+    CreateCommunicationComponent,
 
   ],
   imports: [
