1c35561e39d7540c990e45d513a89140b350fd47
Ganesh0333 <kottaliganesh1994@gmail.com>
2024-06-18 16:12:30 +0530

    view-communiocation-component-implemented

---
+++ src/app/components/view-communications/view-communications.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-communications',
+  templateUrl: './view-communications.component.html',
+  styleUrls: ['./view-communications.component.css']
+})
+export class ViewCommunicationsComponent {
+
+}
