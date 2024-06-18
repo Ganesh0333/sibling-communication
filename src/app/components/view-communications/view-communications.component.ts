f5cedd129116d3819d3f29f8814bd3b8df5f396c
Ganesh0333 <kottaliganesh1994@gmail.com>
2024-06-18 16:14:53 +0530

    view-comminication-implemented

--- src/app/components/view-communications/view-communications.component.ts
+++ src/app/components/view-communications/view-communications.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SiblingServicesService } from 'src/app/services/sibling-services.service';
 
 @Component({
   selector: 'app-view-communications',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./view-communications.component.css']
 })
 export class ViewCommunicationsComponent {
+  public viewtodo:any={}
 
+  constructor(private _siblingService:SiblingServicesService){
+    _siblingService.getviewValue().subscribe(
+      (data:any)=>{
+        this.viewtodo=data
+      }
+    )
+  }
+  
+  edit(data:any){
+    
+  }
 }
